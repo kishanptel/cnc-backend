82650651cf826eb11e451bce349d3e3b0bee212d
Kishan <kishanptel0709@gmail.com>
2026-07-12 23:55:10 +0530

    serverless function error

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
-const { PORT } = require("../config/config")
-const DataBaseConnection = require("../config/database")
-const app = require("./app")
-const UserModel = require("../modules/users/user.model")
+const DataBaseConnection = require("../config/database");
+const app = require("./app");
+const UserModel = require("../modules/users/user.model");
 
-async function ServerStart() {
-    let inConnected = false
+let isConnected = false;
+let adminSeeded = false;
 
+module.exports = async (req, res) => {
     try {
         if (!isConnected) {
-            const connected = await DataBaseConnection()
-            inConnected = true
-            if (!connected) {
-                console.log("Database error check database connection...")
-            } else {
-                // Seed Admin user
-                try {
-                    const adminExist = await UserModel.findOne({ isAdmin: true })
-                    if (!adminExist) {
-                        const admin = new UserModel({
-                            name: "Admin",
-                            email: "admin@sweetshop.in",
-                            password: "admin123",
-                            isAdmin: true,
-                            profile: "/sweet_shop_logo.png"
-                        })
-                        await admin.save()
-                        console.log("Admin account successfully seeded in database.")
-                    }
-                } catch (seedErr) {
-                    console.error("Failed to seed admin account:", seedErr.message)
-                }
-            }
+            await DataBaseConnection();
+            isConnected = true;
         }
-        return app(req, res, next)
-    } catch (error) {
-        console.log("Startup Error", error.message)
+
+        if (!adminSeeded) {
+            const adminExist = await UserModel.findOne({ isAdmin: true });
+
+            if (!adminExist) {
+                await UserModel.create({
+                    name: "Admin",
+                    email: "admin@sweetshop.in",
+                    password: "admin123",
+                    isAdmin: true,
+                    profile: "/sweet_shop_logo.png"
+                });
+
+                console.log("Admin account seeded.");
+            }
+
+            adminSeeded = true;
+        }
+
+        return app(req, res);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error: err.message
+        })
     }
 }
-
-ServerStart()
